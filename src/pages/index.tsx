7cb6615d1310f8417661bfc226a6ead139a0fa92
Anton Paisov <paisov@gmail.com>
2025-08-12 13:23:31 +0100

    added missing 0.7.0 version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
-  return <Redirect to="/running-a-node/setup" />;
+  return <Redirect to="/getting-started/setup" />;
 }
